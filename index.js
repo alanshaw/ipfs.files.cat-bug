05e76d9f9ea19b7f328183508beec093f8bacbf4
Alan Shaw <alan@tableflip.io>
2017-12-15 10:33:20 +0000

    more simples

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const Ipfs = require('ipfs')
 
-function getIpfs () {
+const getIpfs = () => new Promise((resolve, reject) => {
   const ipfs = new Ipfs({ config: { Addresses: { Swarm: [] } } })
-  return ipfs.isOnline()
-    ? Promise.resolve(ipfs)
-    : new Promise((resolve, reject) => {
-      ipfs.on('ready', () => resolve(ipfs)).on('error', reject)
-    })
-}
+  if (ipfs.isOnline()) return resolve(ipfs)
+  ipfs.on('ready', () => resolve(ipfs)).on('error', reject)
+})
 
-(async () => {
+;(async () => {
   console.log('Please wait booting IPFS...')
   const ipfs = await getIpfs()
   const path = '/ipfs/QmfGBRT6BbWJd7yUc2uYdaUZJBbnEFvTqehPFoSMQ6wgdr/tour'
   console.log(`Getting path ${path}`)
   try {
     await ipfs.files.cat(path)
-    console.log('??? should throw')
+    console.error('??? should throw')
   } catch (err) {
     if (err.message.toLowerCase() === 'this dag node is a directory') {
       console.log('Hooray, it work!')
